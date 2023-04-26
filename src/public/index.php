e94d6333aa299b6134f34c4c89837a0561893bbb
Olavo Adriel <ollavoadriel@gmail.com>
2023-04-26 11:02:07 -0300

    implementado HTML ao PHP

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,42 @@
-<?php
+<html>
+    <head>
+        <title> PHP test </title>
+    </head>
+    <body>
+        <?php
+        //declare(strict_types=1);
+        
+            /* $contador = 1;
+            $mensagem = "Olá";
+            $versao = array('1.1','2','3.4');
+            //print_r($versao)
+            $primeiro_nome = "Maria ";
+            echo $primeiro_nome;
+            $ultimo_nome = "\n Lourdes";
+            echo $ultimo_nome 
+            $time = array('Maria', 'Jose', 'Ana');
+            echo $time[2];
+            echo '<br />';
+            print_r($time);
+            
+            $valor1 = 15;
+            $valor1 +=5;
+            
+            echo $valor1;
+            echo 'a: [' . TRUE . ']<br />';
+            echo 'a: [' . FALSE . ']<br />';
+            $idade = 50;
+            
+            echo 'e: [' . $idade . ']<br />'; 
+            */
+            $saldo = 10;
+            if ($saldo<100){
+                $deposito = 1000;
+                $saldo += $deposito;
+            }
+            echo $saldo;
+            
 
-//phpinfo();
-
-echo 'oi';
+            ?>
+    </body>
+</html>
